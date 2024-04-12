6d6c47bf1f35d60706b1dbe181a74da8e7e05635
cathychoysm <123876192+cathychoysm@users.noreply.github.com>
2024-04-12 17:05:31 +0100

    Update projects data

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,5 +1,5 @@
 export const projectUrls = [
-  "https://search-clone-69618.web.app/",
+  "https://search-clone-69618.web.app",
   "https://cathychoysm.github.io/project-expenses-app/",
   "https://cathychoysm.github.io/project-multi-step-form/",
   "https://cathychoysm.github.io/project-star-rating-component/",
